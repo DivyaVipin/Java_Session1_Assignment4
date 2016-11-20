6ee48c9da6fc3639f435adb8d1e53aa2edadd144
DivyaVipin <divyababu1001@gmail.com>
2016-11-20 21:06:41 +0530

    Add files via upload

---
+++ Asci.java
@@ -0,0 +1,18 @@
+
+public class Asci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		for (int a = 65; a <= 90; a++)
+
+		{
+
+			System.out.println("Value of " +a+ " is: " + (char)a);//Print the corresponding Asci Value
+
+		}
+
+	}
+
+}
